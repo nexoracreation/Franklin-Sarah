2d80f6e6d61635d9e63d12de4d5609615b5a6ea6
rohaneldhose2008 <rohaneldhose2008@gmail.com>
2026-09-21 18:16:46 +0530

    Restore gallery-3 and send source and guest count in RSVP payload

--- script.js
+++ script.js
@@ -709,11 +709,13 @@ window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
       submitBtn.style.opacity = '0.7';
 
       var payload = {
+        source: 'sangeet-wedding-site',
         name: name,
         phone: phone,
         attending: 'yes',
         guests: guestCount,
         guestCount: guestCount,
+        totalGuests: guestCount,
         events: checkedEvents,
         message: message
       };
